e7fd1b5b5b2804163018de94cf116802bebbc105
Alp Emre <78408018+alpemre8@users.noreply.github.com>
2025-02-03 15:27:41 +0300

    Create helper_functions.py

---
+++ helpers/helper_functions.py
@@ -0,0 +1,112 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import itertools
+from sklearn.metrics import consufison_matrix
+import pandas as pd
+import tensorflow as tf
+
+
+def plot_decision_boundary(model, X, y):
+
+  x_min, x_max = X[:, 0].min() - 0.1, X[:, 0].max() + 0.1
+  y_min, y_max = X[:, 1].min() - 0.1, X[:, 1].max() + 0.1
+
+  xx, yy = np.meshgrid(np.linspace(x_min, x_max, 100),
+                      np.linspace(y_min, y_max, 100))
+
+  x_in = np.c_[xx.ravel(), yy.ravel()]
+  y_pred = model.predict(x_in)
+
+  if model.output_shape[-1] > 1:
+    y_pred = np.argmax(y_pred, axis=1).reshape(xx.shape)
+
+  else:
+    y_pred = np.round(np.max(y_pred, axis=1)).reshape(xx.shape)
+
+  plt.contourf(xx, yy, y_pred, cmap=plt.cm.RdYlBu, alpha=0.7)
+  plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap=plt.cm.RdYlBu)
+  plt.xlim(xx.lim(), xx.max())
+  plt.ylim(yy.min(), yy.max())
+
+
+
+#-----------------------------------------------------------------------------------------------
+
+
+def make_confusion_matrix(y_true, y_pred, classes=None, figsize=(10, 10), text_size=15):
+
+  cm = confusion_matrix(y_true, y_pred)
+  cm_norm = cm.astype("float") / cm.sum(axis=1)[:, np.newaxis]
+  n_classes = cm.shape[0]
+
+  fig, ax = plt.subplots(figsize=figsize)
+  cax = ax.matshow(cm, cmap=plt.cm.Blues)
+  fig.colorbar(cax)
+
+  if classes:
+    labels = classes
+  else:
+    labels = np.arange(cm.shape[0])
+
+  ax.set(title="Confusion Matrix",
+        xlabel="Predicted label",
+        ylabel="True label",
+        xticks=np.arange(n_classes),
+        yticks=np.arange(n_classes),
+        xticklabels=labels,
+        yticklabels=labels)
+
+
+  ax.xaxis.set_label_position("bottom")
+  ax.xaxis.tick_bottom()
+
+  threshold = (cm.max() + cm.min()) / 2.
+
+  for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+
+    plt.text(j, i, f"{cm[i, j]}({cm_norm[i, j]*100:1.f}%)",
+            horizontalalignment="center",
+            color="white" if cm[i, j] > threshold else "black",
+            size=text_size)
+
+
+
+#---------------------------------------------------------------------------
+
+
+def plot_loss_curves(history):
+
+  loss = history.history["loss"]
+  val_loss = history.history["val_loss"]
+  accuracy = history.history["accuracy"]
+  val_accuracy = history.history["val_accuracy"]
+
+  epochs = range(len(history.history["loss"]))
+
+  plt.plot(epochs, loss, label="training_loss")
+  plt.plot(epochs, val_loss, label="val_loss")
+  plt.title("Loss")
+  plt.xlabel("Epochs")
+  plt.legend()
+
+  plt.figure()
+  plt.plot(epochs, accuracy, label="training_accuracy")
+  plt.plot(epochs, val_accuracy, label="val_accuracy")
+  plt.title("Accuracy")
+  plt.xlabel("Epochs")
+  plt.legend();
+
+
+
+#--------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
